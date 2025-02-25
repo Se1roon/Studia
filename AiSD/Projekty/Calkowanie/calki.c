50fb1912e48a47fb8122c63e713157fd89fec261
Se1roon <se1roonm@pm.me>
2025-02-25 19:27:57 +0100

    AiSD: Poprawki do prostokątów i trapezów, notatki do MC

--- AiSD/Projekty/Calkowanie/calki.c
+++ AiSD/Projekty/Calkowanie/calki.c
@@ -3,7 +3,7 @@
 #include <math.h>
 #include <time.h>
 
-#define lp 1000
+#define lp 2000
 
 double c_od, c_do;
 
@@ -15,36 +15,42 @@ double f4(double x) {
 	return x*x + 2*x;
 }
 
-// To trzeba jakąś inną metodą zrobić xd
 double prostokaty(double (*f)(double)) {
 	double sum = 0;
 	double step = (c_do - c_od) / lp;
 	for (double i = c_od; i < c_do; i += step)
-		sum += f(i) * step;
+		sum += f(i + step/2);
 
-	return sum;
+	return sum * step;
 }
 
 double trapezy(double (*f)(double)) {
-	double sum = 0;
+	double sum = (f(c_do) + f(c_od)) / 2;
 	double step = (c_do - c_od) / lp;
-	for (double i = c_od; i < c_do; i += step) {
-		if (i + step > c_do) sum += (f(i) + f(c_do)) * step / 2;
-		else sum += (f(i) + f(i + step)) * step / 2;
-	}
+	for (double i = c_od + step; i < c_do; i += step)
+		sum += f(i);
 		
-	return sum;
+	return sum * step;
 }
-       
+     
 double mc(double (*f)(double)) {
+	// Znalezc fmin i fmax (przeleciec caly przedzial)
+	// jezeli oba sa > 0 to mniejszy do 0
+	// N razy generujemy x <c_od; c_do> i y <Min; Max>
+	// jezeli y > 0 to +1
+	// jezeli y < 0 to -1
+	// N+/N- = P[]/całka
+
 	double sum = 0;
 	double distance = c_do - c_od;
 
 	srand(time(NULL));
 
 	int i = 1;
-	for (; i <= lp; i++)
-		sum += f(c_od + ((double)rand() / ((double)RAND_MAX + 1) * distance));
+	for (; i <= lp; i++) {
+		double x = f(c_od + ((double)rand() / ((double)RAND_MAX + 1) * distance));
+		sum += x;	
+	}
 
 	sum = distance * sum / lp;
 
